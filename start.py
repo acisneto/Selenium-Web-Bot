4785ed27dcbd6adc038b758c9c527e49ef86a4b2
Francisco de Assis <frcoassis97@gmail.com>
2024-08-08 10:30:22 -0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,23 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from captcha_solver import CaptchaSolver
+
+driver = webdriver. Firefox()
+connectButton = driver.find_element(By.ID, "connectButton") 
+actionchains = ActionChains(driver)
+ActionChains(driver).click(connectButton).perform()
+time.sleep(5)
+driver.get("https://accounts.google.com")
+time.sleep(7)
+
+
+select LogIn = driver.find_element(By.XPATH, "//input[@id='identifierId']") ActionChains (driver).click(selectLogin).perform()
+LogIn = driver.find_element(By.ID, "identifierId")
+LogIn.send_keys("teste")
+LogIn.send_keys(Keys.ENTER)
+time.sleep(5)
+selectCaptcha driver.find_element(By.CLASS_NAME, "TrZEUC")
+selectCaptcha.screenshot("teste.png")
+time.sleep(3)
